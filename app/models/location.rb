dfedb683bd9daaaa88adce12b12a168317fd9203
mgleissn <mgleissn@gmail.com>
2013-07-11 23:29:48 -0500

    Got location option working for server model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
+  has_many :servers
   attr_accessible :building, :name, :room, :room_number, :phone
 end
--- app/models/server.rb
+++ app/models/server.rb
@@ -1,4 +1,4 @@
 class Server < ActiveRecord::Base
   belongs_to :location
-  attr_accessible :name, :os
+  attr_accessible :name, :os, :location_id
 end
